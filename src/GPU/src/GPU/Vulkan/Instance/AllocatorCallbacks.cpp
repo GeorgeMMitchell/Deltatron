3af51e86939782dfbb0464d16538da170afb86fc
George Mitchell <georgemaxwellmitchell@outlook.com>
2022-01-31 18:38:27 -0600

    ignore_unused(m_alloc_clbks)

--- src/GPU/src/GPU/Vulkan/Instance/AllocatorCallbacks.cpp
+++ src/GPU/src/GPU/Vulkan/Instance/AllocatorCallbacks.cpp
@@ -1,4 +1,9 @@
 #include <dt/GPU/Vulkan/Instance/AllocatorCallbacks.hpp>
 
+#include <dt/util/ignore_unused.hpp>
+
 dt::VulkanInstanceAllocatorCallbacks::VulkanInstanceAllocatorCallbacks(System const&)
-: m_alloc_clbks{} {}
+: m_alloc_clbks{}
+{
+  ignore_unused(m_alloc_clbks);
+}
